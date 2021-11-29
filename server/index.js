73f0d05e998d2d97a358ed2a236a9b72acb28fc4
kukuanne <umubyeyiannemarie@gmail.com>
2021-11-29 00:43:32 -0800

    trying to replace ES5 with ES6

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res) => {
+    res.send(helloooo);
+});
+app.listen(5000, () => console.log('listening on port 5000'));
--- server/index/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('hello anne');
-});
-app.listen(5000), () => console.log('listerning on port 5000....')
