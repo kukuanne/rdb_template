e886505c4524460a753a12124a0bf6a9325280e8
kukuanne <umubyeyiannemarie@gmail.com>
2021-11-30 08:01:30 -0800

    i have set RDB's on 5000 & created env file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
 const app = express();
+dotenv.config();
 
-const PORT = 5000;
 
-app.listen(PORT,() => console.log(`Server running on port : http://localhost${PORT}`));
+app.get('/', function(req, res) {
+       res.send('hello anne');
+     });
+
+app.listen(process.env.PORT,() => console.log(`Server running on port ${process.env.PORT}`));
 
 // app.get('/', function(req, res) {
 //     res.send('hello anne');
