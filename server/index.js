08e74b4ba8a2afa5374729920019697909315c1e
kukuanne <umubyeyiannemarie@gmail.com>
2021-12-07 01:41:23 -0800

    GET,POST,PUT working perfectly

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// import joi from 'joi'
+import joi from 'joi'
 import express from 'express'
 import dotenv from 'dotenv'
 
@@ -87,7 +87,28 @@ app.get('/api/drinks/not-allowed', function(req, res) {
 });
 
 
-app.post('/api/citizens/create', (req, res) =>{
+app.post('/api/citizens/create', (req, res) => {
+  const schema = joi.object( {
+    // id:joi.string().min(1).max(2).required(),
+    name:joi.string().min(3).required(),
+    category:joi.string().min(3).required(),
+    active: joi.number().integer().min(1920).max(2021),
+    end: joi.number().integer().min(1920).max(2021),
+    age: joi.number().required()
+  })
+
+
+  const validation = schema.validate(req.body);
+  
+  if(validation.error){
+    res.status(400).send(validation.error.details[0].message);
+    return;
+  }
+  
+
+
+  // res.json({result});
+  
 
 
 
@@ -99,13 +120,66 @@ app.post('/api/citizens/create', (req, res) =>{
      end: req.body.end,
      age: req.body.age
   };
+
+
   citizens.push(citizen);
   res.json({message:'Citizen created sucessful', citizen})
 
-}
+
+});
+
+app.put('/api/citizens/update/:id',(req, res) =>{
+
+  const citizen = citizens.find(c => c.id === parseInt(req.params.id))
+  if(!citizen) res.status(404).json({message:'No citizen with that ID'})
+
+  
+  const schema = joi.object( {
+    // id:joi.string().min(1).max(2).required(),
+    name:joi.string().min(3).required(),
+    category:joi.string().min(3).required(),
+    active: joi.number().integer().min(1920).max(2021),
+    end: joi.number().integer().min(1920).max(2021),
+    age: joi.number().required()
+
+  })
+  const validation = schema.validate(req.body);
+  if(validation.error){
+    res.status(400).send(validation.error.details[0].message);
+    return;
+  }
+ 
+  citizen.name = req.body.name;
+  citizen.category = req.body.category;
+  citizen.active = req.body.active;
+  citizen.end = req.body.end;
+  citizen.age = req.body.age;
+
+  
+  res.json({ message:'citizen updated sucessfull',citizen})
 
 
-)
+});
+
+
+
+
+
+  
+  // return res.status(200).json({
+  //     status:200,
+  //     message:'Citizen account updated',
+  //     citizens: {
+  //       name: updateUserQuery.rows[0].name,
+  //       category: updateUserQuery.rows[0].category,
+  //       active: updateUserQuery.rows[0].active,
+  //       end: updateUserQuery.rows[0].end,
+  //       age: updateUserQuery.rows[0].age,
+        
+  //     }
+  //   }); 
+    // joi.validate(citizen, schema);
+
 
 
 
