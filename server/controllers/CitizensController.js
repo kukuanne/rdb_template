3977bb1b049a6fcbf0d13d61a2b5d39bb89c5f37
kukuanne <umubyeyiannemarie@gmail.com>
2021-12-15 07:22:09 -0800

    CRUD API& Database

--- server/controllers/CitizensController.js
+++ server/controllers/CitizensController.js
@@ -1,67 +1,135 @@
 import joi from 'joi';
+
 import express from 'express';
 import dotenv from 'dotenv';
-import citizens from '../db/citizens';
+import { sequelize,Citizen} from '../models';
+
+
+ 
+
+
 
 
 
 export default class CitizensController {
 
-    static async getAllAllowed(req, res) {
-        const newGreaterData = citizens.filter((citizen) => citizen.age >= 18);
+    // static async getAllAllowed(req, res) {
+    //     const newGreaterData = citizens.filter((citizen) => citizen.age >= 18);
 
-        return res.json({
-            message: 'Citizens allowed to drink',
-            countDrinker: newGreaterData.length,
-            newGreaterData
-        })
+    //     return res.json({
+    //         message: 'Citizens allowed to drink',
+    //         countDrinker: newGreaterData.length,
+    //         newGreaterData
+    //     })
+    // }
+
+    // static async getAllNotAllowed(req, res) {
+    //     const newLessData = citizens.filter((citizen) => citizen.age < 18);
+
+    //     return res.json({
+    //         message: 'Citizens not allowed to drink',
+    //         countNotDrinker: newLessData.length,
+    //         newLessData
+    //     })
+    // }
+
+   static async create(req, res)  {
+      const { name, email, role} = req.body;
+
+      try{
+
+        const citizen = await Citizen.create({name, email,role});
+
+        return res.json({message:'Citizen created successful', citizen})
+      }
+      catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+      }
+
+    }
+    static async getAllCitizens(req, res)  {
+      const { name, email, role} = req.body;
+
+      try{
+
+        const citizen = await Citizen.findAll({name, email,role});
+
+        return res.json({message:'list of all citizens', citizen})
+      }
+      catch(err){
+        console.log(err)
+        return res.status(500).json({error:'something went wrong'})
+      }
+
     }
 
-    static async getAllNotAllowed(req, res) {
-        const newLessData = citizens.filter((citizen) => citizen.age < 18);
+    static async getOneCitizen(req, res)  {
+      const uuid = req.params.uuid;
+      try{
 
-        return res.json({
-            message: 'Citizens not allowed to drink',
-            countNotDrinker: newLessData.length,
-            newLessData
-        })
+        const citizen = await Citizen.findOne({wwhere:{uuid}})
+
+        return res.json({citizen})
+      }
+      catch(err){
+        console.log(err)
+        return res.status(500).json({error:'something went wrong'})
+      }
+
+    }
+    
+    static async update(req, res)  {
+      const uuid = req.params.uuid;
+      try{
+
+        const citizen = await Citizen.findOne({wwhere:{uuid}})
+
+        return res.json({citizen})
+      }
+      catch(err){
+        console.log(err)
+        return res.status(500).json({error:'something went wrong'})
+      }
+
     }
 
-    static async create(req, res){
+    
 
-        const schema = joi.object( {
-            // id:joi.string().min(1).max(2).required(),
-            name:joi.string().min(3).required(),
-            category:joi.string().min(3).required(),
-            active: joi.number().integer().min(1920).max(2021),
-            end: joi.number().integer().min(1920).max(2021),
-            age: joi.number().required()
-          })
+
+
+    //     const schema = joi.object( {
+    //         // id:joi.string().min(1).max(2).required(),
+    //         name:joi.string().min(3).required(),
+    //         category:joi.string().min(3).required(),
+    //         active: joi.number().integer().min(1920).max(2021),
+    //         end: joi.number().integer().min(1920).max(2021),
+    //         age: joi.number().required()
+    //       })
         
         
-        const validation = schema.validate(req.body);
+    //     const validation = schema.validate(req.body);
           
-        if(validation.error){
-            res.status(400).send(validation.error.details[0].message);
-            return;
-        }
+    //     if(validation.error){
+    //         res.status(400).send(validation.error.details[0].message);
+    //         return;
+    //     }
 
             
-        const citizen = {
-            id: citizens.length +1,
-            name: req.body.name,
-            category: req.body.category,
-            active: req.body.active,
-            end: req.body.end,
-            age: req.body.age
+    //     const citizen = {
+    //         id: citizens.length +1,
+    //         name: req.body.name,
+    //         category: req.body.category,
+    //         active: req.body.active,
+    //         end: req.body.end,
+    //         age: req.body.age
 
-    };
+    // };
 
 
-    citizens.push(citizen);
-    res.json({message:'Citizen created sucessful', citizen})
+    // citizens.push(citizen);
 
-    }
+    
 
     static async update(req, res){
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import joi from 'joi';
 import express from 'express';
 import dotenv from 'dotenv';
-import CitizensController from './controllers/CitizensController';
+
+import CitizensController from './controllers/citizensController';
+import { sequelize } from './models';
+
+
+
+
+
 
 
 
 const app = express();
+
 app.use(express.json())
 dotenv.config();
 
@@ -14,17 +22,28 @@ dotenv.config();
      
      
     
+// async function main(){
+//     await sequelize.sync()
+  
+//    }
+
+//    main()
 
 
 
 
 
-app.get('/api/drinks/allowed', CitizensController.getAllAllowed);
-app.get('/api/drinks/not-allowed', CitizensController.getAllNotAllowed); 
+app.get('/api/citizens', CitizensController.getAllCitizens); 
+
+app.get('/api/citizens/:uuid', CitizensController.getOneCitizen);
 app.post('/api/citizens/create', CitizensController.create); 
 app.put('/api/citizens/update/:id', CitizensController.update); 
 app.delete('/api/citizens/delete/:id',CitizensController.delete);
-app.listen(process.env.PORT,() => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, async () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+    await sequelize.authenticate()
+     console.log('Database connected!')
+});
 
 
  
